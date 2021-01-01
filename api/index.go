b0cb25f8a176d76fb9c9ef9057b0fee026ce01f8
TBXark <tbxark@outlook.com>
2021-01-01 23:44:28 +0800

    Initial commit

---
+++ api/index.go
@@ -0,0 +1,44 @@
+package handle
+
+import (
+	"fmt"
+	"github.com/tbxark/g4vercel"
+	"net/http"
+)
+
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	server := gee.Default()
+	server.GET("/", func(context *gee.Context) {
+		context.JSON(200, gee.H{
+			"status": "OK",
+		})
+	})
+	server.GET("/hello", func(context *gee.Context) {
+		name := context.Query("name")
+		if name == "" {
+			context.JSON(400, gee.H{
+				"error": "name not found",
+			})
+		} else {
+			context.JSON(200, gee.H{
+				"data": fmt.Sprintf("Hello %s!", name),
+			})
+		}
+	})
+	server.GET("/user/:id", func(context *gee.Context) {
+		context.JSON(400, gee.H{
+			"data": gee.H{
+				"id": context.Param("id"),
+			},
+		})
+	})
+	server.GET("/long/long/long/path/*test", func(context *gee.Context) {
+		context.JSON(200, gee.H{
+			"data": gee.H{
+				"url": context.Path,
+			},
+		})
+	})
+	server.Handle(w, r)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tbxark/g4vercel-demo
+
+go 1.15
+
+require github.com/tbxark/g4vercel v0.0.0-20210101153229-45f762f00ecb
